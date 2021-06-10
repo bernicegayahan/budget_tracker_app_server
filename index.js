83502ba2e483e82ac5eaaaec31af26e2045a4d79
Marty Miguel <marty.miguel@tuitt.com>
2021-06-10 19:51:59 +0800

    included our env module

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+require('dotenv').config()
 //i want to select a port in which i want to run the project. 
 const PORT = 4000; 
 
 //lets now connect to the our database platform
 //inject a connection string on the first parameter of the connect()
 //dont copy my credentials!
-mongoose.connect(`mongodb+srv://admin:a0Sq5i3hEDnmPOt5@cluster0.qyycl.mongodb.net/budget_tracker_cs3?retryWrites=true&w=majority`, {
+const ConnectionString = process.env.DB_CONNECT
+
+mongoose.connect(ConnectionString, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 })
